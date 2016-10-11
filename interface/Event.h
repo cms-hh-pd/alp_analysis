f3813d764e387833045673f2a99077d411adf634
Pablo de Castro <pablodecm@gmail.com>
2016-10-11 09:35:34 +0200

    read simple Muons and MET information for trigger studies
    Use directly TTreeReaders of ALPHA structs members to read
    the pt and pfIso03 of Muons and MEt.pt.

--- interface/Event.h
+++ interface/Event.h
@@ -2,6 +2,7 @@
 #pragma once
 // ROOT includes
 #include <TTreeReader.h>
+#include <TTreeReaderArray.h>
 // other CMSSW modules includes
 #include "Analysis/ALPHA/interface/alp_objects.h"
 // includes from this repositoty
@@ -19,6 +20,11 @@ namespace alp {
       // objects which will be read from TTree before first operator
       std::vector<std::pair<std::string, bool>> hlt_bits_;
       std::vector<alp::Jet> jets_;
+      // required muon and trigger information for trigger studies
+      // NOTE: might load objects in the future instead 
+      std::vector<float> muons_pt_;
+      std::vector<float> muons_pfiso03_;
+      float met_pt_;
       // additional stuff that might be created during the processing 
       std::vector<PtEtaPhiEVector> dijets_;
       std::vector<std::size_t> free_is_;
@@ -26,13 +32,18 @@ namespace alp {
       // TTreeReaderValue/Array pointers (so they are nullable) to get the data 
       TTreeReaderValue<std::vector<alp::Jet>> * jets_reader_ = nullptr;
       std::vector<std::unique_ptr<TTreeReaderValue<bool>>> hlt_bits_reader_; 
+      TTreeReaderArray<float> * muons_pt_reader_ = nullptr;
+      TTreeReaderArray<float> * muons_pfiso03_reader_ = nullptr;
+      TTreeReaderValue<float> * met_pt_reader_ = nullptr;
   
       Event() {}
       Event(TTreeReader & reader, const json & config = {}) {
+        // load jet collection
         if (config.find("jets_branch_name") != config.end()) {
             jets_reader_ = new TTreeReaderValue<std::vector<alp::Jet>>(reader, 
                 config.at("jets_branch_name").get_ref<const std::string &>().c_str());
         }
+        // load trigger bits
         if (config.find("hlt_names") != config.end()) {
           for (const auto & hlt_name : config.at("hlt_names")) {
             const std::string & hlt_name_s = hlt_name.get_ref<const std::string &>(); 
@@ -41,10 +52,19 @@ namespace alp {
                 hlt_name_s.c_str()));
           }
         }
+
+        // muon and met information required for trigger
+        muons_pt_reader_ = new TTreeReaderArray<float>(reader, "Muons.pt");
+        muons_pfiso03_reader_ = new TTreeReaderArray<float>(reader, "Muons.pfIso03");
+        met_pt_reader_ = new TTreeReaderValue<float>(reader, "MEt.pt");
+
       }
   
       virtual ~Event() {
         delete jets_reader_;
+        delete muons_pt_reader_;
+        delete muons_pfiso03_reader_;      
+        delete met_pt_reader_;
       }
 
       virtual void update() {
@@ -56,6 +76,16 @@ namespace alp {
         for (std::size_t i=0; i<hlt_bits_reader_.size(); i++) {
           hlt_bits_.at(i).second = **(hlt_bits_reader_.at(i));
         }
+
+        // muon information
+        muons_pt_.clear();
+        for (std::size_t i=0; i<muons_pt_reader_->GetSize(); i++) {
+          muons_pt_.emplace_back(muons_pt_reader_->At(i));
+          muons_pfiso03_.emplace_back(muons_pfiso03_reader_->At(i));
+        }
+
+        // met information
+        met_pt_ = **met_pt_reader_;
 
       }
   
