e32c139e24a0b67c7c853b27274ab84c443ab238
MDallOsso <martino.dall'osso@cern.ch>
2017-02-16 16:52:30 +0100

    updated rew

--- interface/ReWeightingOperator.h
+++ interface/ReWeightingOperator.h
@@ -15,8 +15,9 @@
       std::string filew_name_;
       std::string osample_;
       std::vector<std::string> sam_list_;
+      std::vector<std::string> hist_list_;
       TFile filew_;
-      TH1F hw_;      
+      std::vector<TH1F> hw_;      
 
       ReWeightingOperator(std::string filew_name, std::string osample) :
       filew_name_(filew_name),
@@ -25,11 +26,17 @@
         sam_list_ = {"SM","BM2","BM3","BM4","BM5","BM6",
                     "BM7","BM8","BM9","BM10","BM11","BM12","BM13","BMbox"};
 
+        //DEBUG - edit with same order of same list!
+        hist_list_ = {"SM","BM2","BM3","BM4","BM5","BM6",
+                    "BM7","BM8","BM9","BM10","BM11","BM12","BM13","BMbox"}; 
+
         //get histogram related to sample
         std::string hanme = osample; //DEBUG
         filew_ = TFile::Open(filew_name.c_str());
-        hw_ = (TH1F*)filew_.Get(hanme.c_str());
-
+        for (int i=0; i<hist_list_.size(); i++) {
+            TH1F h = (TH1F)filew_.Get(hist_list_.at(i).c_str());
+            hw_.push_back(h); 
+        }
       }
 
       virtual ~ReWeightingOperator() {}
@@ -45,7 +52,7 @@
           weight_map["ReWeighting_"+sam] = 1.0;
         }
 
-        for (const auto & sam : sam_list_) {
+        for (unsigned int i=0; i<sam_list_.size(); i++) {
 
             //example on how to call ev variables
             float costh = ev.tl_genhh_.at(0).costhst();
@@ -56,9 +63,9 @@
             float mergecostSum = 0;
            // for ii in range(1,11) : mergecostSum+= sumHBenchBin.GetBinContent(bmhh,ii)  DEBUG
             if (mergecostSum >0) {
-                weight = (hw_.GetBinContent(mhh,costh) / mergecostSum);
+                weight = (hw_.at(i).GetBinContent(mhh,costh) / mergecostSum);
             }
-            weight_map.at("ReWeighting_"+sam) *= weight;           
+            weight_map.at("ReWeighting_"+sam_list_.at(i)) *= weight;           
         }
 
         // add weights to event info
