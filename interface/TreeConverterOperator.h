108c0b09349ef9fc90711e308fcc2d9d8dd700b6
MDallOsso <martino.dall'osso@cern.ch>
2017-04-06 10:19:22 +0200

    script to get plain tree for ttH studies

--- interface/TreeConverterOperator.h
+++ interface/TreeConverterOperator.h
@@ -15,44 +15,82 @@ template <class EventClass> class TreeConverterOperator : public BaseOperator<Ev
   public:
  
     // variables to save in branches
-    float_t jet0_pt = 0.;
-    float_t jet0_eta = 0.;
-    float_t jet0_csv = 0.;
-
-    float_t dijet0_pt = 0.;
-    float_t dijet0_eta = 0.;
-    float_t dijet0_phi = 0.;
-    float_t dijet0_energy = 0.;
-
-    float_t dijet1_pt = 0.;
-    float_t dijet1_eta = 0.;
-    float_t dijet1_phi = 0.;
-    float_t dijet1_energy = 0.;
-
+    float_t weight = 0.;
     std::vector<std::string> weights_;
+
+    std::vector<float_t>   jets_pt;
+    std::vector<float_t> jets_eta;
+    std::vector<float_t> jets_cmva;
+    std::vector<float_t> jets_csv;
+    std::vector<float_t> jets_phi;
+    std::vector<float_t> jets_mass;
+
+    std::vector<float_t> muons_pt;
+    std::vector<float_t> muons_eta;
+    std::vector<float_t> muons_phi;
+    std::vector<float_t> muons_mass;
+    std::vector<float_t> muons_iso03;
+
+    std::vector<float_t> elecs_pt;
+    std::vector<float_t> elecs_eta;
+    std::vector<float_t> elecs_phi;
+    std::vector<float_t> elecs_mass;
+
+    std::vector<float_t> genBfromH_pt;
+    std::vector<float_t> genBfromH_eta;
+    std::vector<float_t> genBfromH_phi;
+    std::vector<float_t> genBfromH_mass;
+
+    float_t met_pt;
+    float_t met_phi;
+
+    json config_ = {};
 
     TTree tree_{"tree","Tree using  dataformats"};
 
-    TreeConverterOperator(const std::vector<std::string> & weights = {}) :
+    TreeConverterOperator(const std::string & config_s, const std::vector<std::string> & weights = {}) :
+      config_(json::parse(config_s)), 
       weights_(weights) {}
-
     virtual ~TreeConverterOperator() {}
 
     virtual void init(TDirectory * tdir) {
+      if (config_.find("jets_branch_name") != config_.end()) {
+        tree_.Branch("jets_pt","std::vector<float>",&jets_pt, 64000, 2);
+        tree_.Branch("jets_eta","std::vector<float>",&jets_eta, 64000, 2);
+        tree_.Branch("jets_phi","std::vector<float>",&jets_phi, 64000, 2);
+        tree_.Branch("jets_mass","std::vector<float>",&jets_mass, 64000, 2);
+        tree_.Branch("jets_csv","std::vector<float>",&jets_csv, 64000, 2);
+        tree_.Branch("jets_cmva","std::vector<float>",&jets_cmva, 64000, 2);
+      }
 
-      tree_.Branch("jet0_pt", &jet0_pt, "jet0_pt/F");
-      tree_.Branch("jet0_eta", &jet0_eta, "jet0_eta/F");
-      tree_.Branch("jet0_csv", &jet0_csv, "jet0_csv/F");
+      if (config_.find("muons_branch_name") != config_.end()) {
+        tree_.Branch("muons_pt","std::vector<float>",&muons_pt, 64000, 2);
+        tree_.Branch("muons_eta","std::vector<float>",&muons_eta, 64000, 2);
+        tree_.Branch("muons_phi","std::vector<float>",&muons_phi, 64000, 2);
+        tree_.Branch("muons_mass","std::vector<float>",&muons_mass, 64000, 2);
+        tree_.Branch("muons_iso03","std::vector<float>",&muons_iso03, 64000, 2);
+     }
 
-      tree_.Branch("dijet0_pt",   &dijet0_pt,   "dijet0_pt/F");
-      tree_.Branch("dijet0_eta",  &dijet0_eta,  "dijet0_eta/F");
-      tree_.Branch("dijet0_phi",  &dijet0_phi,  "dijet0_phi/F");
-      tree_.Branch("dijet0_energy",    &dijet0_energy, "dijet0_energy/F");
+      if (config_.find("electrons_branch_name") != config_.end()) {
+        tree_.Branch("elecs_pt","std::vector<float>",&elecs_pt, 64000, 2);
+        tree_.Branch("elecs_eta","std::vector<float>",&elecs_eta, 64000, 2);
+        tree_.Branch("elecs_phi","std::vector<float>",&elecs_phi, 64000, 2);
+        tree_.Branch("elecs_mass","std::vector<float>",&elecs_mass, 64000, 2);
+     }
 
-      tree_.Branch("dijet1_pt",   &dijet1_pt,   "dijet1_pt/F");
-      tree_.Branch("dijet1_eta",  &dijet1_eta,  "dijet1_eta/F");
-      tree_.Branch("dijet1_phi",  &dijet1_phi,  "dijet1_phi/F");
-      tree_.Branch("dijet1_energy",    &dijet1_energy, "dijet1_energy/F");
+      if (config_.find("genbfromhs_branch_name") != config_.end()) {
+        tree_.Branch("genBfromH_pt","std::vector<float>",&genBfromH_pt, 64000, 2);
+        tree_.Branch("genBfromH_eta","std::vector<float>",&genBfromH_eta, 64000, 2);
+        tree_.Branch("genBfromH_phi","std::vector<float>",&genBfromH_phi, 64000, 2);
+        tree_.Branch("genBfromH_mass","std::vector<float>",&genBfromH_mass, 64000, 2);
+     }
+
+      if (config_.find("met_branch_name") != config_.end()) {
+        tree_.Branch("met_pt", &met_pt, "met_pt/F");
+        tree_.Branch("met_phi", &met_phi, "met_phi/F");
+     }
+  //   tree_.Branch("trigger", &trigger, "trigger/F");
+      tree_.Branch("weight", &weight, "weight/F");
 
       tree_.SetDirectory(tdir);
       tree_.AutoSave();
@@ -61,20 +99,70 @@ template <class EventClass> class TreeConverterOperator : public BaseOperator<Ev
 
 
     virtual bool process( EventClass & ev ) {
-      
-      jet0_pt  =  ev.jets_.at(0).pt();
-      jet0_eta =  ev.jets_.at(0).eta();
-      jet0_csv =  ev.jets_.at(0).CSV();
 
-      dijet0_pt =  ev.dijets_.at(0).pt();
-      dijet0_eta =  ev.dijets_.at(0).eta();
-      dijet0_phi =  ev.dijets_.at(0).phi();
-      dijet0_energy =  ev.dijets_.at(0).energy();
+      jets_pt.clear();
+      jets_eta.clear();
+      jets_cmva.clear();
+      jets_csv.clear();
+      jets_phi.clear();
+      jets_mass.clear();
 
-      dijet1_pt =  ev.dijets_.at(1).pt();
-      dijet1_eta =  ev.dijets_.at(1).eta();
-      dijet1_phi =  ev.dijets_.at(1).phi();
-      dijet1_energy =  ev.dijets_.at(1).energy();
+      muons_pt.clear();
+      muons_eta.clear();
+      muons_phi.clear();
+      muons_mass.clear();
+      muons_iso03.clear();
+
+      elecs_pt.clear();
+      elecs_eta.clear();
+      elecs_phi.clear();
+      elecs_mass.clear();
+
+      genBfromH_pt.clear();
+      genBfromH_eta.clear();
+      genBfromH_phi.clear();
+      genBfromH_mass.clear();
+
+
+      float w = 1.0;
+      w*=ev.eventInfo_.eventWeight(weights_);
+      weight = w;
+
+      for (std::size_t i = 0; i < ev.jets_.size(); i++ ) {
+        jets_pt.push_back(ev.jets_.at(i).pt());
+        jets_eta.push_back(ev.jets_.at(i).eta());
+        jets_phi.push_back(ev.jets_.at(i).phi());
+        jets_mass.push_back(ev.jets_.at(i).mass());
+        jets_csv.push_back(ev.jets_.at(i).disc("pfCombinedInclusiveSecondaryVertexV2BJetTags"));
+        jets_cmva.push_back(ev.jets_.at(i).disc("pfCombinedMVAV2BJetTags"));
+     }
+
+/*      for (std::size_t i = 0; i < ev.elecs_.size(); i++ ) {
+        elecs_pt.push_back(ev.elecs_.at(i).pt());
+        elecs_eta.push_back(ev.elecs_.at(i).eta());
+        elecs_phi.push_back(ev.elecs_.at(i).phi());
+        elecs_mass.push_back(ev.elecs_.at(i).mass());
+        elecs_csv.push_back(ev.elecs_.at(i).csv());
+        elecs_cmva.push_back(ev.elecs_.at(i).cmva());
+      }*/
+
+      for (std::size_t i = 0; i < ev.genbfromhs_.size(); i++ ) {
+        genBfromH_pt.push_back(ev.genbfromhs_.at(i).pt());
+        genBfromH_eta.push_back(ev.genbfromhs_.at(i).eta());
+        genBfromH_phi.push_back(ev.genbfromhs_.at(i).phi());
+        genBfromH_mass.push_back(ev.genbfromhs_.at(i).mass());
+     }
+
+      for (std::size_t i = 0; i < ev.muons_.size(); i++ ) {
+        muons_pt.push_back(ev.muons_.at(i).pt());
+        muons_eta.push_back(ev.muons_.at(i).eta());
+        muons_phi.push_back(ev.muons_.at(i).phi());
+        muons_mass.push_back(ev.muons_.at(i).mass());
+        muons_iso03.push_back(ev.muons_.at(i).iso03());
+      }
+
+      met_pt = ev.met_.pt();
+      met_phi = ev.met_.phi();
 
       tree_.Fill();
 
